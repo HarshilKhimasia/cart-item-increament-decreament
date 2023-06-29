d46b4432c8844600f0442e94720ab8381fed3a54
Harshil Khimasia <118171733+HarshilKhimasia@users.noreply.github.com>
2023-06-29 23:20:06 +0530

    created new branch, discount button feature started (#1)

--- script.js
+++ script.js
@@ -1,24 +1,47 @@
 'use strict';
 
-const increaseBtn = document.querySelector('.increase-button');
-const decreaseBtn = document.querySelector('.decrease-button');
+let increaseBtn = document.querySelector('.increase-button');
+let decreaseBtn = document.querySelector('.decrease-button');
 let counter = document.querySelector('.counter-para');
 let clearBtn = document.querySelector('.clear-button');
-counter.textContent = 0;
+let priceUpdate = document.querySelector('.price-update');
+let discountPara = document.querySelector('.discount');
+let discountBtn = document.querySelector('.discount-button');
+let finalPrice;
 
+Number(discountPara.textContent);
+Number((counter.textContent = 0));
+
+priceUpdate.textContent = 0;
+let fixedPrice = 1200;
 let increaseFunc = function () {
-  counter.textContent++;
+  Number(counter.textContent++);
+  priceUpdate.textContent = Number(counter.textContent) * fixedPrice;
 };
 let decreaseFunc = function () {
   if (counter.textContent > 0) {
     counter.textContent--;
+    priceUpdate.textContent = Number(counter.textContent) * fixedPrice;
   }
 };
 
 let clearFunc = function () {
   counter.textContent = 0;
+  priceUpdate.textContent = 0;
+  discountPara.textContent = undefined;
 };
+
+// let discFunc = function () {
+//   priceUpdate.textContent;
+//   finalPrice = (20 / 100) * priceUpdate.textContent;
+//   discountPara.textContent = priceUpdate.textContent - finalPrice;
+
+//   if (discountPara.textContent > 0) {
+//     increaseBtn.disabled = true;
+//   }
+// };                    TODO: //  resume discount functionality (6/29/23)
 
 increaseBtn.addEventListener('click', increaseFunc);
 decreaseBtn.addEventListener('click', decreaseFunc);
 clearBtn.addEventListener('click', clearFunc);
+// discountBtn.addEventListener('click', discFunc); TODO: //  resume discount functionality (6/29/23)
